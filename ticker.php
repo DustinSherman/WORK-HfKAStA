334f56485ac416c5de21e863ee69b25e3f31e62e
jelko arnds <moin@jelkoarnds.com>
2020-09-15 15:43:31 +0200

    Ticker: Externe Links, Link to Post, No Link, Custom Ticker Message

--- ticker.php
+++ ticker.php
@@ -6,30 +6,21 @@
     )); 
 ?>
 
-
 <div class="ticker-wrap">
     <div class="ticker" id="ticker">
         <?php if ( $the_query->have_posts() ) : ?>
             <?php for ($i = 0 ; $i < 3; $i++) : ?>
                 <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                    <?php $message = get_field('ticker_message') ? get_field('ticker_message') : get_the_title(); ?>
                     <div class="news">
-                    <?php if ( get_field('news_link') ) : ?>
-                        <a href="<?php the_field('news_link'); ?>" 
-                            <?php
-                                $newTab = get_field('new_tab');
-
-                                if( $newTab && in_array('ja', $newTab) ) {
-                                    echo 'target="_blank"';
-                                }
-                            ?>
-                        >
-                    <?php endif; ?>
-                        <?php the_excerpt(); ?>
-                    <?php if ( get_field('news_link') ) : ?>                
-                        </a>
-                    <?php endif; ?>
+                        <?php /* Link wenn, der post keinen Inhalt hat oder der externe Link gesetzt ist. */ ?>
+                        <?php if ( '' !== get_post()->post_content || !empty(get_post_meta($post->ID,'me_spr_post_redirect',true)) ) : ?>
+                            <a href="<?php echo get_permalink(); ?>"><p><?php echo $message; ?></p></a>
+                        <?php else : ?>
+                            <p><?php echo $message; ?></p>
+                        <?php endif; ?>
                     </div>
-                    <div class="news_seperator">+++</div>
+                    <div class="news_seperator"> +++ </div>
                 <?php endwhile; ?>
             <?php endfor; ?>
             <?php wp_reset_postdata(); ?>
