65c95dbd167df54b3d9763090e2996f62c65eda9
jelko arnds <moin@jelkoarnds.com>
2020-09-17 15:15:15 +0200

    Sidebar: Content auch für Category und Posts.

--- template-parts/sidebar-content.php
+++ template-parts/sidebar-content.php
@@ -1,17 +1,23 @@
-<?php if (get_field('sidebar_note')) : ?>
+<?php
+    $sidebar_note = get_field('sidebar_note');
+    if(empty($sidebar_note)){
+        if (is_page()){
+            $object_id = wp_get_post_parent_id($post);
+        }elseif(is_single()){
+            $object_id = get_the_category() ? get_the_category()[0] : false;
+        }else{
+            $object_id = $wp_query->get_queried_object();
+        }
+
+        if ($object_id){
+            $sidebar_note = get_field('sidebar_note', $object_id);
+        }
+    }
+
+    if ($sidebar_note): ?>
     <div class="sidebar_note">
         <p>
-            <?php the_field('sidebar_note'); ?>
+            <?php echo $sidebar_note; ?>
         </p>
     </div>
-<?php elseif (is_page()) :
-    $parentID = $post->post_parent;
-    ?>
-    <?php if (get_field('sidebar_note', $parentID)) : ?>
-        <div class="sidebar_note">
-            <p>
-                <?php the_field('sidebar_note', $parentID); ?>
-            </p>
-        </div>
-    <?php endif; ?>
 <?php endif; ?>
